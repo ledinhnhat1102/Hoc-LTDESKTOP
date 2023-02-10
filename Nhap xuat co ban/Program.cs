139865dfb0d4ccc202d3dd765dcb7849d9c82eb4
ledinhnhat1102 <nhat6870k@gmail.com>
2023-02-11 01:06:31 +0700

    hh

---
+++ Nhap xuat co ban/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nhap_xuat_co_ban
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Le Dinh Nhat");
+            //Console.WriteLine("hihi");
+            //Console.WriteLine("Le Dinh Nhat");
+            //Console.WriteLine("Le Dinh Nhat 2");
+            //int a = 5;
+            //Console.WriteLine("a = "+a);
+            //Console.WriteLine("{0} {1} {2}", 99, 77, 33);
+            
+            //Console.ReadKey();
+
+<<<<<<< HEAD
+            //doc 1 ki tu
+            //Console.WriteLine(Console.Read());
+            //Console.ReadKey();
+
+            //doc 1 dog
+            //Console.WriteLine(Console.ReadLine());
+            //Console.ReadKey();
+
+            //Console.WriteLine("le dinh nhat");
+
+            //Console.ReadKey();
+            //Console.ReadKey(false);
+            //Console.ReadKey(true);
+
+            Console.WriteLine("le dinh nhat");
+            Console.Write("moi ban nha ho ten cua ban: ");
+            Console.WriteLine("ten cua ban la: " + Console.ReadLine());
+
+            Console.Write("moi ban nhap que quan: ");
+            Console.WriteLine("que quan : " + Console.ReadLine());
+            
+=======
+            //nhap
+            Console.Write(Console.Read());
+            Console.ReadKey();
+            //cc
+            //jj
+>>>>>>> 17b3837ebf3a1888d8e607e749a116e270efbabb
+        }
+    }
+}
